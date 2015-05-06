877bd1809bf1c968f0239839125e3aca07b595e9
conansherry <conansherry@vip.qq.com>
2015-05-06 10:54:03 +0800

    fix text

--- calcWeiboFlowers.py
+++ calcWeiboFlowers.py
@@ -18,7 +18,7 @@ from bs4 import BeautifulSoup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0',
-    'cookie': 'your cokie'
+    'cookie': 'your cookies'
 }
 
 idol_array = [
